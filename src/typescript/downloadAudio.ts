35394669e3d924589892d2da5a06d9f89646992e
Ivan Kapitanov <ivorybananaa@gmail.com>
2023-03-31 23:19:32 +0300

    Stream parts can be acquired by the extension

--- src/typescript/downloadAudio.ts
+++ src/typescript/downloadAudio.ts
@@ -1,36 +1,53 @@
-import { AudioData, ReloadAudiosResponse, TransformData } from './types';
+import { AudioData, SegmentsInfo } from './types';
 import getM3U8Url from '../vk_source/getM3U8Url';
+import { getStreamComponent, getTransformData } from './api_calls';
+import { getSegmentsInfo } from './M3U8_parser';
 
-const GET_AUDIO_DATA = 'al_audio.php?act=reload_audios';
+const KEY_BYTE_LENGTH = 16;
+const CRYPT_ALGO = 'AES-CBC';
+
+async function getSegments(
+  streamUrl: string,
+  key: CryptoKey,
+  segmentsInfo: SegmentsInfo
+): Promise<ArrayBuffer[]> {
+  const segments = await Promise.all<Promise<ArrayBuffer>>(
+    segmentsInfo.map(async ({ isEncrypted, segIdx }) => {
+      const segment = await getStreamComponent(streamUrl, 'segment', segIdx);
+
+      if (isEncrypted) {
+        const iv = getIV(segIdx);
+        const decryptedSegment = await crypto.subtle.decrypt(
+          { name: CRYPT_ALGO, iv: iv },
+          key,
+          segment
+        );
+        return decryptedSegment;
+      }
+      return segment;
+    })
+  );
+  return segments;
+}
+
+async function getKey(streamUrl: string): Promise<CryptoKey> {
+  const key_bytes = await getStreamComponent(streamUrl, 'decrypt_key');
+  const key = await crypto.subtle.importKey('raw', key_bytes, CRYPT_ALGO, false, ['decrypt']);
+
+  return key;
+}
+
+function getIV(segIdx: number): Int8Array {
+  const iv = new Int8Array(KEY_BYTE_LENGTH);
+  iv.set([segIdx], 15);
+
+  return iv;
+}
 
 const getStreamUrl = (apiUnavailableUrl: string, vk_id: number): string => {
   const m3u8Url = getM3U8Url(apiUnavailableUrl, vk_id);
   const idx = m3u8Url.lastIndexOf('/');
   return m3u8Url.substring(0, idx + 1);
-};
-
-const getContentData = async (audioID: string): Promise<TransformData | never> => {
-  const fd = new FormData();
-  fd.set('al', '1');
-  fd.set('audio_ids', audioID);
-
-  const response = await fetch(GET_AUDIO_DATA, {
-    method: 'POST',
-    body: fd,
-    headers: {
-      'x-requested-with': 'XMLHttpRequest',
-    },
-  });
-
-  if (response.ok) {
-    const result: ReloadAudiosResponse = await response.json();
-    return {
-      vk_id: result.payload[1][0][0][15].vk_id,
-      apiUnavailableUrl: result.payload[1][0][0][2],
-    };
-  }
-
-  throw new Error(response.statusText);
 };
 
 const getAudioID = (rawAudioData: string): string => {
@@ -40,9 +57,16 @@ const getAudioID = (rawAudioData: string): string => {
 
 const downloadAudio = async (rawAudioData: string) => {
   const audioID = getAudioID(rawAudioData);
-  const { vk_id, apiUnavailableUrl } = await getContentData(audioID);
+  const { vk_id, apiUnavailableUrl } = await getTransformData(audioID);
   const streamUrl = getStreamUrl(apiUnavailableUrl, vk_id);
-  console.log(streamUrl);
+
+  const playlist = await getStreamComponent(streamUrl, 'playlist');
+  const segmentsInfo = getSegmentsInfo(playlist);
+
+  const key = await getKey(streamUrl);
+
+  const segments = await getSegments(streamUrl, key, segmentsInfo);
+  console.log(segments);
 };
 
 export default downloadAudio;
